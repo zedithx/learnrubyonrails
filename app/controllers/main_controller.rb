f8b34ac56542682931e3cc0102fdf24adf1d5673
Yang Si Jun <aersijun@gmail.com>
2024-05-04 19:26:44 +0800

    added flashcards

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,4 +1,6 @@
 class MainController < ApplicationController
     def index
+        # flashcard, allows for dynamic population of info, like a prop
+        flash[:notice] = "Logged in successfully"
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  get "about", to: "about#index"
-  get "/", to: "main#index"
+  # gets /about through the index func in about controller, let prefix remain as about
+  get "about", to: "about#index", as: :about
+  root to: "main#index"
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
