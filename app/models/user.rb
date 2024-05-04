d17527b585ae40779e3b5e0fe4eaf13edf8d7ce7
Yang Si Jun <aersijun@gmail.com>
2024-05-04 19:44:08 +0800

    model migration and create user in console

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+# email: string
+# password_digest:string (converted from password using bcrypt)
+
+# password: string virtual
+# password_confirmation: string virtual
+
+class User < ApplicationRecord
+    # adds security and authentication, bcrypt + verify
+    has_secure_password
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
   # gets /about through the index func in about controller, let prefix remain as about
   get "about", to: "about#index", as: :about
+  
   root to: "main#index"
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
